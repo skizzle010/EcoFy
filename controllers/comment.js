d22ce20cdc489b0b93da8da540f7ed0e639eacae
Your Name <pavdarsh@gmail.com>
2023-06-08 06:30:19 +0530

    try-catch-fix

--- controllers/comment.js
+++ controllers/comment.js
@@ -3,64 +3,83 @@ const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
 
 async function create_comment(req, res) {
-    id = req.id;
+  id = req.id;
   const { comment } = req.body;
   try {
     const newComment = await prisma.comment.create({
       data: {
         comment: comment,
-        connect:{
-            User:{
-                id:id
-            }
-        }
+        connect: {
+          User: {
+            id: id,
+          },
+        },
       },
     });
-    return res.status(200).json({"message":"commented"},newComment)
-  } 
-  catch (error) {
-    return res.status.json({"message":"post does not exist"})
+    return res.status(200).json({ message: "commented" }, newComment);
+  } catch (error) {
+    return res.status.json({ message: "post does not exist" });
   }
 }
 
-async function comment_delete_one(req,res){
+async function comment_delete_one(req, res) {
   id = req.id;
-  const c_id = req.params.c_id;
-  await prisma.comment.delete({where:{commentId:c_id}})
-  return res.status(200).json({"message":"comment deleted"})
+  try {
+    const c_id = req.params.c_id;
+    await prisma.comment.delete({ where: { commentId: c_id } });
+    return res.status(200).json({ message: "comment deleted" });
+  } catch (error) {
+    return res.status(500).json("comment does not exist");
+  }
 }
 
-async function comment_delete_many(req,res){
+async function comment_delete_many(req, res) {
   id = req.id;
-  await prisma.comment.deleteMany({where:{userId:id}})
-  return res.status(200).json({"message":"all comment deleted"})
+  try {
+    await prisma.comment.deleteMany({ where: { userId: id } });
+    return res.status(200).json({ message: "all comment deleted" });
+  } catch (error) {
+    return res.status(400).json("user does not exist");
+  }
 }
 
-
-async function get_comment(req,res){
+async function get_comment(req, res) {
+  try {
     const commentId = req.params.id;
-    const data = await prisma.comment.findUnique({where:{commentId:commentId}})
-    return res.status(200).json(data)
+    const data = await prisma.comment.findUnique({
+      where: { commentId: commentId },
+    });
+    return res.status(200).json(data);
+  } catch (error) {
+    return res.status(401).json("comment does not exist");
+  }
 }
 
-async function get_all_comments(req,res){
-    id = req.id;
-    const data = await prisma.comment.findMany({where:{userId:id}})
-    return res.status(200).json(data)
+async function get_all_comments(req, res) {
+  id = req.id;
+  try {
+    const data = await prisma.comment.findMany({ where: { userId: id } });
+    return res.status(200).json(data);
+  } catch (error) {
+    return res.status(404).json("user does not exist");
+  }
 }
 
-
-
-async function update_comment(req,res){
-    id = req.id;
-    const{comment} = req.body;
-    const newComment = await prisma.comment.update({where:{userId:id},
-    data:{
-        comment:comment
-    }})
-    return res.status(200).json(newComment)
+async function update_comment(req, res) {
+  id = req.id;
+  try {
+    const { comment } = req.body;
+    const newComment = await prisma.comment.update({
+      where: { userId: id },
+      data: {
+        comment: comment,
+      },
+    });
+    return res.status(200).json(newComment);
+  } catch (error) {
+    return res.status("comment does not exist");
+  }
 }
-
 
 module.exports = {
   create_comment,
@@ -68,6 +87,5 @@ module.exports = {
   comment_delete_many,
   get_comment,
   get_all_comments,
-  update_comment
-
+  update_comment,
 };
--- routes/post.js
+++ routes/post.js
@@ -1,13 +1,14 @@
 const app = require("express")
 const router = app.Router();
+const Auth = require("../middleware/auth.js")
 
 const {create_post, getmyposts, get_post, update_post,deleteOne,deleteall } = require("../controllers/post")
 
-router.post("/", create_post);
-router.get("/", getmyposts);
+router.post("/",Auth.verifyToken, create_post);
+router.get("/",Auth.verifyToken, getmyposts);
 router.get("/:id", get_post);
-router.put("/:id", update_post);
-router.delete("/:id", deleteOne);
-router.delete("/", deleteall);
+router.put("/:id",Auth.verifyToken, update_post);
+router.delete("/:id",Auth.verifyToken, deleteOne);
+router.delete("/",Auth.verifyToken, deleteall);
 
 module.exports = router;
